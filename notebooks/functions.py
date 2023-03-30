b0691aa7fac732c4703195c90868f4d135f58f76
Terentev Nikita <nitro-2003@mail.ru>
2023-03-30 20:04:46 +0700

    Added comments on what needs to be fixed.

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,41 +1,42 @@
 import matplotlib.pyplot as plt
 
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_curve, auc
+from matplotlib.ticker import MultipleLocator
 
 
-def rock_rating_curve_charts(predictor, title, x_train, y_train, x_test,
-                             y_test):
-    train_pred_proba = predictor.predict_proba(x_train)
-    test_pred_proba = predictor.predict_proba(x_test)
+def plot_roc_curves(ax, probs, names, y_test, axes):
+    """
+    Строит ROC кривые для всех моделей
+    :param ax: ось, на которой строим график
+    """
+    labels = tuple()
+    for index, prob in enumerate(probs):
+        fpr, tpr, _ = roc_curve(y_test, prob)
+        area = round(auc(fpr, tpr), 3)
+        labels += (names[index] + f" ({area=})",)
+        axes[0].plot(fpr, tpr)
+    ax.plot((-0.01, 1.01), (-0.01, 1.01), color="navy", linestyle="--")
+    ax.set_title("ROC curves")
+    ax.set_xlim((-0.01, 1.01))
+    ax.set_ylim((-0.01, 1.01))
+    ax.set_xlabel("False positive rate", labelpad=15)
+    ax.set_ylabel("True positive rate", labelpad=15)
+    ax.legend(loc="lower right", labels=labels)
 
-    resp = {
-        "train_pred_proba": train_pred_proba[:, 1],
-        "test_pred_proba": test_pred_proba[:, 1],
-        "train_score": roc_auc_score(y_train, train_pred_proba[:, 1]),
-        "test_score": roc_auc_score(y_test, test_pred_proba[:, 1])
-    }
 
-    train_curve = roc_curve(y_train, resp["train_pred_proba"])
-
-    test_curve = roc_curve(y_test, resp["test_pred_proba"])
-
-    # строим график ROC-кривых
-    plt.figure(figsize=(12, 6))
-    plt.plot(*train_curve[:2],
-             label='Train ROC curve (AUC={:.2f})'.format(resp["train_score"]))
-    plt.plot(*test_curve[:2],
-             label='Validation ROC curve (AUC={:.2f})'.format(
-                 resp["test_score"]))
-
-    # добавляем диагональную линию
-    plt.plot([0, 1], [0, 1], 'k--', lw=2)
-
-    # настраиваем параметры графика
-    plt.xlim([-0.05, 1.05])
-    plt.ylim([-0.05, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title(title)
-    plt.legend(loc='lower right')
-    plt.show()
+def plot_bar_graph(ax, names, scoring):
+    """
+    Строит столбчатую диаграмму для score моделей
+    :param ax: ось, на которой строим график
+    """
+    ax.bar(names, scoring)
+    ax.set_title("Scoring (ROC-AUC)")
+    ax.tick_params("x", labelrotation=90)
+    ax.set_ylim((0.5, 1))
+    ax.yaxis.set_major_locator(MultipleLocator(0.05))
+    ax.yaxis.set_minor_locator(MultipleLocator(0.01))
+    for rect, score in zip(ax.patches, scoring):
+        ax.text(rect.get_x() + rect.get_width() / 2, rect.get_height() + 0.01,
+                score, ha="center")
+    ax.set_xlabel("Model", labelpad=15)
+    ax.set_ylabel("Score", labelpad=15)
